8d2b1304d683ae20de24128cfed21201ed303f53
alang <arthur.lang@epitech.eu>
2021-02-11 17:54:50 +0100

    add decision tree

---
+++ src/DecisionTree.py
@@ -0,0 +1,24 @@
+#
+# Created on Thu Feb 11 2021
+#
+# Arthur Lang
+# DecisionTree.py
+#
+from AAlgorithm import AAlgorithm
+
+class DecisionTree(AAlgorithm):
+
+    def __init__(self):
+        super().__init__("Descicion tree")
+
+    def fit(self, data, target):
+        pass
+
+    def predict(self, testSample):
+        pass
+
+    def predict_proba(self, testSample):
+        pass
+
+    def plotTree(self, toPlot):
+        pass
