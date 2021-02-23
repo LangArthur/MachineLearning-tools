6a2b618c977059dff49e78113d0e1ac8e93ebba4
alang <arthur.lang@epitech.eu>
2021-02-23 17:45:19 +0100

    add scaler

--- src/MyNeuralNetwork.py
+++ src/MyNeuralNetwork.py
@@ -9,18 +9,32 @@ import sys
 
 from src.AAlgorithm import AAlgorithm
 
+class Node():
+
+    def __init__(self, weights):
+        self.weights = weights
+        self.output = 0
+        self.delta = 0 # reprensent backward propagation error
+
+    def activate(self):
+        pass
+
+# class Layer():
+#      def __init__(self, nbNode)
+
 class MyNeuralNetwork(AAlgorithm):
 
     def __init__(self):
         super().__init__("Neural-Network")
         self._layerNb = 1 # number of hidden layers in the network
         self._nbNeurone = 10 # number of neurones in each layers
+        self._weights = []
 
     def setHiddenLayerNb(self, nb):
-        if (nb > 0): # TODO check if can work with 0
+        if (nb > 0):
             self._layerNb = nb
         else:
-            print("Error: invalide number of layers: " + str(nb) + ".", file=sys.stderr)
+            print("Error: invalide number of layers: " + str(nb) + " is not > 0.", file=sys.stderr)
 
     def setNbNeurones(self, nb):
         if (nb > 1): # TODO check if can work with 1
@@ -28,6 +42,8 @@ class MyNeuralNetwork(AAlgorithm):
         else:
             print("Error: invalide number of neurones: " + str(nb) + ".", file=sys.stderr)
 
+    def setWeights(self, weights):
+        self._weights = weights
 
     def fit(self, data, targets):
         pass
