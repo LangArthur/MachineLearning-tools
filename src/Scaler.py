232b3002581a32e28202a0f9cb8c1c1d277248f2
alang <arthur.lang@epitech.eu>
2021-02-25 11:08:11 +0100

    rework normalize function to scaler

--- src/Scaler.py
+++ src/Scaler.py
@@ -10,26 +10,10 @@ import copy
 
 class Scaler():
 
-    def __init__(self):
-        self._minVals = []
-        self._maxVals = []
+    def _normalize(self, array):
+        maxVal = numpy.max(array)
+        minVal = numpy.min(array)
+        return (array - maxVal) / (maxVal - minVal)
 
-    def fit(self, data):
-        if (len(data) < 1):
-            raise RuntimeError("Error: no data pass to fit scaler.")
-        self._minVals = copy.copy(data[0])
-        self._maxVals = copy.copy(data[0])
-        for elem in data:
-            for i, value in enumerate(elem):
-                if (self._minVals[i] > value):
-                    self._minVals[i] = value
-                if (self._maxVals[i] < value):
-                    self._maxVals[i] = value
-
-    def _normalize(self, value, pos):
-        return value - self._minVals[pos] / self._maxVals[pos] - self._minVals[pos]
-
-    def transform(self, data):
-        numpy.vectorize(data)
-        # https://www.oreilly.com/library/view/python-for-data/9781449323592/ch04.html
-        return (numpy.array([i for i, x in enumerate(data)]))
+    def normalize(self, array, axe):
+        return numpy.apply_along_axis(self._normalize, axe, array)
